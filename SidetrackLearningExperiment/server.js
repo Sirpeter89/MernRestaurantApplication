082fe5686b8b985db9e91280b9379f578c6c1a3c
Justin <just2rules@gmail.com>
2022-11-09 16:36:46 -0800

    learned how to incorporate ejs

--- SidetrackLearningExperiment/server.js
+++ SidetrackLearningExperiment/server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 const app = express()
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
     console.log('Hello World')
     //Can send status codes
@@ -8,7 +10,11 @@ app.get('/', (req, res) => {
     //Can chain statuses
     // res.sendStatus(500).send("Internal server error encountered")
     // res.sendStatus(500).json({ message: "Error"})
-    res.send('Hi')
+    //you can even send downloads
+    //res.download('path to file)
+    //can render files
+    res.render('index', { text: 'World' })
+    // res.send('Hi')
 })
 
 app.listen(3000)
